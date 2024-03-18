60509bc599fa528f346dcec82d9561ea808786ac
Mana Allaire <manaallaire@outlook.com>
2024-03-19 00:27:55 +0800

    feat(endpoints): make object immutable

--- src/constants/endpoints.ts
+++ src/constants/endpoints.ts
@@ -1,10 +1,10 @@
-export const endpoints = {
-  search: 'search?',
-  featured: 'featured?',
-  categories: 'categories?',
-  searchSuggestions: 'search_suggestions?',
-  autocomplete: 'autocomplete?',
-  trendingSearchTerms: 'trending_terms?',
-  registerShare: 'registershare?',
-  posts: 'posts?',
-};
+export const endpoints = Object.freeze({
+  search: 'search',
+  featured: 'featured',
+  categories: 'categories',
+  searchSuggestions: 'search_suggestions',
+  autocomplete: 'autocomplete',
+  trendingSearchTerms: 'trending_terms',
+  registerShare: 'registershare',
+  posts: 'posts',
+});
